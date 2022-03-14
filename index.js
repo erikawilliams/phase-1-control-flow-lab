a665be8ba174393605e6742d83270b2a20eba4f6
Erika Williams <erika@erikawilliams.us>
2022-03-14 14:09:25 -0600

    commit tree 9451eaa5b64c058e40d2af838433ccf7ad30c82e

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(value){
   // Write your code here!
-}
+  if (value <= 400) {
+    return 'This one is on me!';
+  } else if (value > 2500) {
+      return 'No can do.';
+  } else if (value > 2000) {
+      return 'I will gladly take your thirty bucks.';
+  }
+  }
+  
+  /*
+  switch (value) {
+    case value <= 400:
+      return 'This one is on me!';
+      break;
+    case value > 2500:
+      return 'No can do.';
+      break;
+    case value > 2000:
+      return 'I will gladly take your thirty bucks.';
+  }*/
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(value){
   // Write your code here!
+  return value === 'NYC'?'Ok, sounds good.':'No go.'; 
+} 
+
+function switchOnCharmFromTip(value){
+  // Write your code here!
+  switch (value) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
